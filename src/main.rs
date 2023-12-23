e35bb32e5eec3ce2b57c7f3b87725a97b2dcf990
Dan McArdle <d@nmcardle.com>
2023-12-23 10:52:58 -0500

    Play with Display impl for Token. Factor out token.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,13 @@
 #![feature(custom_inner_attributes)]
 #![feature(stmt_expr_attributes)]
 
+mod token;
+
+use token::Token;
+use token::ParseError;
+
 use std::fmt::Display;
 use std::io::Write;
-
-#[derive(Debug, PartialEq)]
-enum Token<'a> {
-    Num(i32),
-    String(&'a str),
-    Symbol(&'a str),
-    LeftParen,
-    RightParen,
-    SingleQuote,
-}
-
-#[derive(Debug, PartialEq)]
-enum ParseError {
-    ParseNum,
-    Generic,
-    EmptyString,
-    UnterminatedString,
-}
-
-impl Display for ParseError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ParseError::ParseNum => write!(f, "Error parsing number"),
-            ParseError::Generic => write!(f, "Generic error"),
-            ParseError::EmptyString => write!(f, "Expected token"),
-            ParseError::UnterminatedString => write!(f, "Unterminated string literal"),
-        }
-    }
-}
-
-impl Token<'_> {
-    pub fn lex(code: &str) -> Result<Vec<Token>, ParseError> {
-        let mut out = Vec::new();
-        let mut token_buf: &str = &code;
-        while token_buf.len() > 0 && token_buf != "\n" {
-            let (token, tail) = Token::eat_token(&token_buf)?;
-            out.push(token);
-            token_buf = tail;
-        }
-        Ok(out)
-    }
-
-    fn eat_token(s: &str) -> Result<(Token, &str), ParseError> {
-        if s.len() == 0 {
-            return Err(ParseError::EmptyString);
-        }
-        let mut chars = s.chars().peekable();
-        match chars.peek().ok_or(ParseError::Generic)? {
-            ' ' => Token::eat_token(&s[1..]),
-            '-' | '0'..='9' => Token::eat_num_token(&s),
-            '"' => Token::eat_string_token(&s[1..]),
-            'A'..='Z' | 'a'..='z' | '_' => Token::eat_symbol_token(&s),
-            '(' => Ok((Token::LeftParen, &s[1..])),
-            ')' => Ok((Token::RightParen, &s[1..])),
-            '\'' => Ok((Token::SingleQuote, &s[1..])),
-            _ => Err(ParseError::Generic),
-        }
-    }
-
-    fn eat_num_token(s: &str) -> Result<(Token, &str), ParseError> {
-        assert_ne!(s.len(), 0);
-        println!("eat_num_token: {}", s);
-
-        let is_negative = s.chars().next().unwrap() == '-';
-        if let Some(c) = s.chars().skip(1).next() {
-            if c == '-' {
-                return Err(ParseError::ParseNum);
-            }
-        }
-
-        let mut chars = s.chars();
-        if is_negative {
-            chars.next();
-        }
-
-        let orig_len = chars.as_str().len();
-
-        let mut value: i32 = 0;
-        let mut suffix = s;
-        while let Some(c) = chars.next() {
-            match c {
-                '0'..='9' => {
-                    value = 10 * value + ((c as i32) - ('0' as i32));
-                    suffix = chars.as_str();
-                }
-                _ => break,
-            }
-        }
-
-        if chars.as_str().len() == orig_len {
-            Err(ParseError::ParseNum)
-        } else {
-            if is_negative {
-                value *= -1;
-            }
-            Ok((Token::Num(value), suffix))
-        }
-    }
-
-    /// Consume the remainder of a string literal, assuming the opening
-    /// quotation mark has already been consumed.
-    fn eat_string_token(s: &str) -> Result<(Token, &str), ParseError> {
-        let (i_end, _) = s
-            .char_indices()
-            .fold((None, false), |(i_end, is_escaping), (i, c)| {
-                match (i_end, c, is_escaping) {
-                    #![rustfmt::skip]
-                    // If we've already found the end, pass it on.
-                    (Some(i),    _,           _) => (Some(i),        false),
-                    // If we found a quotation mark, we may have found the end
-                    // of the string literal!
-                    (      _,  '"',       false) => (Some(i),        false),
-                    (      _,  '"',        true) => (   None,        false),
-                    // Backslashes are never the end of a string literal, but
-                    // they do affect escaping. If we were already escaping,
-                    // this is a true backlsash. Otherwise, escape the next
-                    // character.
-                    (      _, '\\', is_escaping) => (   None, !is_escaping),
-                    // Regular characters have no special semantics. Disable
-                    // escaping and move on.
-                                               _ => (   None,        false),
-                }
-            });
-        match i_end {
-            Some(i) => Ok((Token::String(&s[..i]), &s[i + 1..])),
-            _ => Err(ParseError::UnterminatedString),
-        }
-    }
-
-    fn eat_symbol_token(s: &str) -> Result<(Token, &str), ParseError> {
-        let len = s
-            .chars()
-            .take_while(|&c| c.is_alphanumeric() || c == '_')
-            .count();
-        Ok((Token::Symbol(&s[..len]), &s[len..]))
-    }
-}
 
 #[derive(Debug)]
 enum RuntimeError {
@@ -180,24 +48,13 @@ impl Display for Expr {
             Expr::String(s) => write!(f, "\"{}\"", s),
             Expr::Symbol(s) => write!(f, "'{}", s),
             Expr::Application(e, args) => {
-                write!(f, "(")?;
-                write!(f, "{}", e)?;
-                write!(
-                    f,
-                    "{}",
-                    args.iter()
-                        .map(|a| format!("{}", a))
-                        .fold(String::new(), |a, b| format!("{} {}", a, b))
-                )?;
-                write!(f, ")")
+                let args_repr = String::from_iter(args.iter().map(|a| format!(" {}", a)));
+                write!(f, "({}{})", e, &args_repr)
             }
             Expr::Quoted(exprs) => write!(
                 f,
-                "(quote {})",
-                exprs
-                    .iter()
-                    .map(|a| format!("{}", a))
-                    .fold(String::new(), |a, b| format!("{} {}", a, b))
+                "(quote{})",
+                String::from_iter(exprs.iter().map(|a| format!(" {}", a)))
             ),
         }
     }
@@ -376,76 +233,54 @@ mod tests {
     use super::*;
 
     #[test]
-    fn simple_tokenizing() {
-        assert_eq!(Token::eat_token("123 )"), Ok((Token::Num(123), " )")));
-        assert_eq!(Token::eat_token("(foo)"), Ok((Token::LeftParen, "foo)")));
-        assert_eq!(Token::eat_token(" foo )"), Ok((Token::Symbol("foo"), " )")));
-        assert_eq!(Token::eat_token("foo )"), Ok((Token::Symbol("foo"), " )")));
+    fn test_display_expr_int() {
+        assert_eq!(format!("{}", Expr::Int(0)), "0");
+        assert_eq!(format!("{}", Expr::Int(1)), "1");
+        assert_eq!(format!("{}", Expr::Int(123)), "123");
+        assert_eq!(format!("{}", Expr::Int(-123)), "-123");
+    }
+
+    #[test]
+    fn test_display_expr_string() {
+        assert_eq!(format!("{}", Expr::String("".to_string())), "\"\"");
+        assert_eq!(format!("{}", Expr::String("abc".to_string())), "\"abc\"");
+        assert_eq!(format!("{}", Expr::String("\\".to_string())), "\"\\\"");
+    }
+
+    #[test]
+    fn test_display_expr_symbol() {
+        // TODO Disallow the empty symbol.
+        assert_eq!(format!("{}", Expr::Symbol("".to_string())), "'");
+        assert_eq!(format!("{}", Expr::Symbol("foo".to_string())), "'foo");
+    }
+
+    #[test]
+    fn test_display_expr_application() {
         assert_eq!(
-            Token::eat_token(" \"abc\")"),
-            Ok((Token::String("abc"), ")"))
+            format!(
+                "{}",
+                Expr::Application(
+                    Box::new(Expr::Symbol("add".to_string())),
+                    vec![Expr::Int(1), Expr::Int(2)]
+                )
+            ),
+            "('add 1 2)"
         );
     }
 
     #[test]
-    fn test_lex() {
+    fn test_display_expr_quoted() {
         assert_eq!(
-            Token::lex("(print 123 \"abc\")"),
-            Ok(vec![
-                Token::LeftParen,
-                Token::Symbol("print"),
-                Token::Num(123),
-                Token::String("abc"),
-                Token::RightParen,
-            ])
+            format!(
+                "{}",
+                Expr::Quoted(vec![
+                    Expr::Symbol("f".to_string()),
+                    Expr::Int(1),
+                    Expr::Int(2)
+                ])
+            ),
+            "(quote 'f 1 2)"
         );
-    }
-
-    #[test]
-    fn test_tokenize_num() {
-        assert_eq!(Token::lex("0"), Ok(vec![Token::Num(0)]));
-        assert_eq!(Token::lex("123"), Ok(vec![Token::Num(123)]));
-        assert_eq!(Token::lex("-0"), Ok(vec![Token::Num(0)]));
-        assert_eq!(Token::lex("-123"), Ok(vec![Token::Num(-123)]));
-        assert_eq!(Token::lex("--123"), Err(ParseError::ParseNum));
-        assert_eq!(Token::lex("---123"), Err(ParseError::ParseNum));
-
-        // TODO: support hexadecimal number literals.
-        assert_eq!(
-            Token::lex("0x123"),
-            Ok(vec![Token::Num(0), Token::Symbol("x123")])
-        );
-    }
-
-    #[test]
-    fn test_tokenize_string() {
-        // Empty string.
-        assert_eq!(Token::lex("\"\""), Ok(vec![Token::String("")]));
-        // Single character strings.
-        assert_eq!(Token::lex("\"a\""), Ok(vec![Token::String("a")]));
-        assert_eq!(Token::lex("\" \""), Ok(vec![Token::String(" ")]));
-        // Strings with spaces.
-        assert_eq!(Token::lex("\" a \""), Ok(vec![Token::String(" a ")]));
-        assert_eq!(Token::lex("\"a \""), Ok(vec![Token::String("a ")]));
-        assert_eq!(Token::lex("\" a\""), Ok(vec![Token::String(" a")]));
-        // Multi-character strings with no spaces.
-        assert_eq!(Token::lex("\"foo\""), Ok(vec![Token::String("foo")]));
-        // Unterminated string literals.
-        assert_eq!(Token::lex("\""), Err(ParseError::UnterminatedString));
-        assert_eq!(Token::lex("\"foo"), Err(ParseError::UnterminatedString));
-    }
-
-    #[test]
-    fn test_tokenize_string_escaping() {
-        assert_eq!(Token::lex("\""), Err(ParseError::UnterminatedString));
-        assert_eq!(Token::lex("\"\\"), Err(ParseError::UnterminatedString));
-        assert_eq!(Token::lex("\"foo\\\""), Err(ParseError::UnterminatedString));
-        assert_eq!(
-            Token::lex("\"foo\\\"\""),
-            Ok(vec![Token::String("foo\\\"")])
-        );
-        assert_eq!(Token::lex(r#""\\\""#), Err(ParseError::UnterminatedString));
-        assert_eq!(Token::lex(r#""\\\\""#), Ok(vec![Token::String(r#"\\\\"#)]));
     }
 
     #[test]
---
+++ src/token.rs
@@ -0,0 +1,215 @@
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    ParseNum,
+    Generic,
+    EmptyString,
+    UnterminatedString,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::ParseNum => write!(f, "Error parsing number"),
+            ParseError::Generic => write!(f, "Generic error"),
+            ParseError::EmptyString => write!(f, "Expected token"),
+            ParseError::UnterminatedString => write!(f, "Unterminated string literal"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Token<'a> {
+    Num(i32),
+    String(&'a str),
+    Symbol(&'a str),
+    LeftParen,
+    RightParen,
+    SingleQuote,
+}
+
+impl Token<'_> {
+    pub fn lex(code: &str) -> Result<Vec<Token>, ParseError> {
+        let mut out = Vec::new();
+        let mut token_buf: &str = &code;
+        while token_buf.len() > 0 && token_buf != "\n" {
+            let (token, tail) = Token::eat_token(&token_buf)?;
+            out.push(token);
+            token_buf = tail;
+        }
+        Ok(out)
+    }
+
+    fn eat_token(s: &str) -> Result<(Token, &str), ParseError> {
+        if s.len() == 0 {
+            return Err(ParseError::EmptyString);
+        }
+        let mut chars = s.chars().peekable();
+        match chars.peek().ok_or(ParseError::Generic)? {
+            ' ' => Token::eat_token(&s[1..]),
+            '-' | '0'..='9' => Token::eat_num_token(&s),
+            '"' => Token::eat_string_token(&s[1..]),
+            'A'..='Z' | 'a'..='z' | '_' => Token::eat_symbol_token(&s),
+            '(' => Ok((Token::LeftParen, &s[1..])),
+            ')' => Ok((Token::RightParen, &s[1..])),
+            '\'' => Ok((Token::SingleQuote, &s[1..])),
+            _ => Err(ParseError::Generic),
+        }
+    }
+
+    fn eat_num_token(s: &str) -> Result<(Token, &str), ParseError> {
+        assert_ne!(s.len(), 0);
+
+        let is_negative = s.chars().next().unwrap() == '-';
+        if let Some(c) = s.chars().skip(1).next() {
+            if c == '-' {
+                return Err(ParseError::ParseNum);
+            }
+        }
+
+        let mut chars = s.chars();
+        if is_negative {
+            chars.next();
+        }
+
+        let orig_len = chars.as_str().len();
+
+        let mut value: i32 = 0;
+        let mut suffix = s;
+        while let Some(c) = chars.next() {
+            match c {
+                '0'..='9' => {
+                    value = 10 * value + ((c as i32) - ('0' as i32));
+                    suffix = chars.as_str();
+                }
+                _ => break,
+            }
+        }
+
+        if chars.as_str().len() == orig_len {
+            Err(ParseError::ParseNum)
+        } else {
+            if is_negative {
+                value *= -1;
+            }
+            Ok((Token::Num(value), suffix))
+        }
+    }
+
+    /// Consume the remainder of a string literal, assuming the opening
+    /// quotation mark has already been consumed.
+    fn eat_string_token(s: &str) -> Result<(Token, &str), ParseError> {
+        let (i_end, _) = s
+            .char_indices()
+            .fold((None, false), |(i_end, is_escaping), (i, c)| {
+                match (i_end, c, is_escaping) {
+                    #![rustfmt::skip]
+                    // If we've already found the end, pass it on.
+                    (Some(i),    _,           _) => (Some(i),        false),
+                    // If we found a quotation mark, we may have found the end
+                    // of the string literal!
+                    (      _,  '"',       false) => (Some(i),        false),
+                    (      _,  '"',        true) => (   None,        false),
+                    // Backslashes are never the end of a string literal, but
+                    // they do affect escaping. If we were already escaping,
+                    // this is a true backlsash. Otherwise, escape the next
+                    // character.
+                    (      _, '\\', is_escaping) => (   None, !is_escaping),
+                    // Regular characters have no special semantics. Disable
+                    // escaping and move on.
+                                               _ => (   None,        false),
+                }
+            });
+        match i_end {
+            Some(i) => Ok((Token::String(&s[..i]), &s[i + 1..])),
+            _ => Err(ParseError::UnterminatedString),
+        }
+    }
+
+    fn eat_symbol_token(s: &str) -> Result<(Token, &str), ParseError> {
+        let len = s
+            .chars()
+            .take_while(|&c| c.is_alphanumeric() || c == '_')
+            .count();
+        Ok((Token::Symbol(&s[..len]), &s[len..]))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_tokenizing() {
+        assert_eq!(Token::eat_token("123 )"), Ok((Token::Num(123), " )")));
+        assert_eq!(Token::eat_token("(foo)"), Ok((Token::LeftParen, "foo)")));
+        assert_eq!(Token::eat_token(" foo )"), Ok((Token::Symbol("foo"), " )")));
+        assert_eq!(Token::eat_token("foo )"), Ok((Token::Symbol("foo"), " )")));
+        assert_eq!(
+            Token::eat_token(" \"abc\")"),
+            Ok((Token::String("abc"), ")"))
+        );
+    }
+
+    #[test]
+    fn test_lex() {
+        assert_eq!(
+            Token::lex("(print 123 \"abc\")"),
+            Ok(vec![
+                Token::LeftParen,
+                Token::Symbol("print"),
+                Token::Num(123),
+                Token::String("abc"),
+                Token::RightParen,
+            ])
+        );
+    }
+
+    #[test]
+    fn test_tokenize_num() {
+        assert_eq!(Token::lex("0"), Ok(vec![Token::Num(0)]));
+        assert_eq!(Token::lex("123"), Ok(vec![Token::Num(123)]));
+        assert_eq!(Token::lex("-0"), Ok(vec![Token::Num(0)]));
+        assert_eq!(Token::lex("-123"), Ok(vec![Token::Num(-123)]));
+        assert_eq!(Token::lex("--123"), Err(ParseError::ParseNum));
+        assert_eq!(Token::lex("---123"), Err(ParseError::ParseNum));
+
+        // TODO: support hexadecimal number literals.
+        assert_eq!(
+            Token::lex("0x123"),
+            Ok(vec![Token::Num(0), Token::Symbol("x123")])
+        );
+    }
+
+    #[test]
+    fn test_tokenize_string() {
+        // Empty string.
+        assert_eq!(Token::lex("\"\""), Ok(vec![Token::String("")]));
+        // Single character strings.
+        assert_eq!(Token::lex("\"a\""), Ok(vec![Token::String("a")]));
+        assert_eq!(Token::lex("\" \""), Ok(vec![Token::String(" ")]));
+        // Strings with spaces.
+        assert_eq!(Token::lex("\" a \""), Ok(vec![Token::String(" a ")]));
+        assert_eq!(Token::lex("\"a \""), Ok(vec![Token::String("a ")]));
+        assert_eq!(Token::lex("\" a\""), Ok(vec![Token::String(" a")]));
+        // Multi-character strings with no spaces.
+        assert_eq!(Token::lex("\"foo\""), Ok(vec![Token::String("foo")]));
+        // Unterminated string literals.
+        assert_eq!(Token::lex("\""), Err(ParseError::UnterminatedString));
+        assert_eq!(Token::lex("\"foo"), Err(ParseError::UnterminatedString));
+    }
+
+    #[test]
+    fn test_tokenize_string_escaping() {
+        assert_eq!(Token::lex("\""), Err(ParseError::UnterminatedString));
+        assert_eq!(Token::lex("\"\\"), Err(ParseError::UnterminatedString));
+        assert_eq!(Token::lex("\"foo\\\""), Err(ParseError::UnterminatedString));
+        assert_eq!(
+            Token::lex("\"foo\\\"\""),
+            Ok(vec![Token::String("foo\\\"")])
+        );
+        assert_eq!(Token::lex(r#""\\\""#), Err(ParseError::UnterminatedString));
+        assert_eq!(Token::lex(r#""\\\\""#), Ok(vec![Token::String(r#"\\\\"#)]));
+    }
+}
